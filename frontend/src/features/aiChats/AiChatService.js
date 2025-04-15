43950ad35204083d0d91095278e14946e52a7334
riskibarqy <riskibarqy@gmail.com>
2025-04-15 15:14:54 +0700

    fix : remove debug

--- frontend/src/features/aiChats/AiChatService.js
+++ frontend/src/features/aiChats/AiChatService.js
@@ -1,18 +1,17 @@
 export const postChatCompletion = async (prompt = "") => {
-    // const res = await fetch("http://localhost:8000/api/ai", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({ prompt }),
-    // });
+    const res = await fetch("http://localhost:8000/api/ai", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt }),
+    });
   
-    // const data = await res.json();
+    const data = await res.json();
   
-    // if (!res.ok || !data.response.success) {
-    //   throw new Error(data.response?.error?.message || "Failed to fetch AI response");
-    // }
-    // return data.response.result;
-    return "HOHOHOHOHO"
+    if (!res.ok || !data.response.success) {
+      throw new Error(data.response?.error?.message || "Failed to fetch AI response");
+    }
+    return data.response.result;
   };
   
